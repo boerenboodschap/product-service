f0b7ccec743a4bcfedd89ea98b4d0ab3b151ab9f
TeunSpithoven <teun.spithoven@gmail.com>
2024-01-01 22:48:04 +0100

    fake metrics for monitoring

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Hosting;
 using MassTransit;
 using Consumers;
 
+using Prometheus;
+using Sample.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Database
@@ -50,6 +53,22 @@ builder.Services.AddMassTransit(config =>
     }
 });
 
+// monitoring
+// Define an HTTP client that reports metrics about its usage, to be used by a sample background service.
+builder.Services.AddHttpClient(SampleService.HttpClientName);
+
+// Export metrics from all HTTP clients registered in services
+builder.Services.UseHttpClientMetrics();
+
+// A sample service that uses the above HTTP client.
+builder.Services.AddHostedService<SampleService>();
+
+builder.Services.AddHealthChecks()
+    // Define a sample health check that always signals healthy state.
+    .AddCheck<SampleHealthCheck>(nameof(SampleHealthCheck))
+    // Report health check results in the metrics output.
+    .ForwardToPrometheus();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -60,9 +79,33 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Capture metrics about all received HTTP requests.
+app.UseHttpMetrics();
+
+#pragma warning disable ASP0014 // Suggest using top level route registrations
+app.UseEndpoints(endpoints =>
+{
+    // Enable the /metrics page to export Prometheus metrics.
+    // Open http://localhost:5099/metrics to see the metrics.
+    //
+    // Metrics published in this sample:
+    // * built-in process metrics giving basic information about the .NET runtime (enabled by default)
+    // * metrics from .NET Event Counters (enabled by default, updated every 10 seconds)
+    // * metrics from .NET Meters (enabled by default)
+    // * metrics about requests made by registered HTTP clients used in SampleService (configured above)
+    // * metrics about requests handled by the web app (configured above)
+    // * ASP.NET health check statuses (configured above)
+    // * custom business logic metrics published by the SampleService class
+    endpoints.MapMetrics();
+});
+#pragma warning restore ASP0014 // Suggest using top level route registrations
 
 app.Run();
 
